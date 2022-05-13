3013a9572ff0bdf511f2fc07b10d50e917f2c19e
Lyubmila <Lyubmila@gmail.com>
2022-05-12 22:08:44 -0400

    some additions..

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let countPlayer = document.querySelector('#count-player')
 let countComputer = document.querySelector('#count-computer')
-
+let computer_choice = document.querySelector('.computer-choices')
+let input = document.querySelector('#textWinner')
 let reset = document.querySelector('#playAgain')
 let choices = document.querySelectorAll('.choice')
-
-//let winner = getWinner(playerChoice, computerChoice)
+let sound = document.querySelector('.sound')
 
 let winner = ''
 let countP = 0
@@ -21,38 +21,85 @@ function play (e) {
 }
 
 function countWinner (winner) {
-    //console.log(winner);
+
+    let text = document.createElement('h1')
+    text.className = "textWinner"
+
+    if (input.innerHTML !== ''){
+        input.removeChild(input.firstChild)
+    }
+    
     if (winner === 'player') {
         
         countP++
         countPlayer.textContent = countP
-        //console.log(winner);
+
+        // sound.setAtribute('src', 'audio/win.mp3')
+        // sound.play()
         
-        // console.log(countPlayer);
-        // countPlayer.textContent = Number(countPlayer.textContent) + 1
-        // console.log(countPlayer);
+        text.textContent = "YOU WON THIS ROUND!!!"
+        text.style.color = 'hsl(141, 100%, 40%)'
+        input.appendChild(text)
+                
     } else if (winner === 'computer') {
 
         countC++
         countComputer.textContent = countC
-        
 
-        //countComputer.textContent = Number(countComputer) + 1
+        // sound.setAtribute('src', 'audio/loss.mp3')
+        // sound.play()
+
+        text.textContent = "YOU LOST THIS ROUND!!!"
+        text.style.color = 'red'
+        input.appendChild(text)
+        
+        //countComputer.textContent = Number(countComputer.textContent) + 1
+    } else {
+        // sound.setAtribute('src', 'audio/draw.mp3')
+        // sound.play()
+
+        text.textContent = "DRAW"
+        text.style.color = 'yellow'
+        input.appendChild(text)
     }
-        console.log(winner);
 }
 
 
 function getComputerChoice () {
-    let rand = Math.floor(Math.random() * 3)
-    if (rand === 1 ) {
-        return 'rock'
-    } else if ( rand === 2 ) {
-        return 'paper'
-    } else {
-        return 'scissors'
+
+    let img = document.createElement("img")
+    img.className = "computerChoices"
+    
+    //remove appendchild img
+    if (computer_choice.innerHTML !== '') {
+        computer_choice.removeChild(computer_choice.firstChild);
     }
 
+    let rand = Math.floor(Math.random() * 3)
+    if (rand === 1 ) {
+        
+
+        img.src = './images/rock.png'
+        computer_choice.appendChild(img)
+
+        return 'rock'
+
+    } else if ( rand === 2 ) {
+        
+
+        img.src = `./images/paper.png`
+        computer_choice.appendChild(img)
+
+        return 'paper'
+
+    } else {
+        
+        img.src = `./images/scissors.png`
+        computer_choice.appendChild(img)
+
+        return 'scissors'
+    }
+    
 }
 
 function getWinner (p, c) {
@@ -81,6 +128,13 @@ function playAgain () {
 
     countP = 0
     countC = 0
+
+    if (computer_choice.innerHTML !== '' && input.innerHTML !== '') {
+        computer_choice.removeChild(computer_choice.firstChild);
+        input.removeChild(input.firstChild)
+    }
+
+    
  }
 
 choices.forEach(choice => choice.addEventListener('click', play))
